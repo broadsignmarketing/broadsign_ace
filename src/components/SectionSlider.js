d968ec45b86efb28fe4d2655bab9ed828b633768
LeKevoid <gagnon.kev@gmail.com>
2019-10-06 15:42:56 -0400

    Progress

---
+++ src/components/SectionSlider.js
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from "react";
+// import Slider from "react-slick";
+// import { graphql } from "gatsby";
+
+// import Img from "gatsby-image/withIEPolyfill";
+
+function SectionSlider(props, { data }) {
+	const {main, sub} = props;
+
+	return (
+		<div className="inner">
+			<h1>{main}</h1>
+			<h2>{sub}</h2>
+		</div>
+	)
+}
+
+export default SectionSlider;
+
+/*
+export const sectionContent = graphql `
+	query Sections {
+
+	}
+`
+*/
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,22 @@
 import React, { useState, useEffect } from "react";
 import Slider from "react-slick";
 import { graphql } from "gatsby";
+import classnames from "classnames";
 
 import Layout from "../components/layout";
 import Img from "gatsby-image/withIEPolyfill";
+import SectionSlider from "../components/SectionSlider";
 
 import "../styles/styles.css";
 
 function IndexPage({ data }) {
-	const [section, setSection] = useState("none");
+	const [section, setSection] = useState("products");
+	const [subSection, setSubSection] = useState("none");
+	const [clientsSliderActive, setClientsSliderActive] = useState("");
 
-	console.log(data);
-
-	const openSection = () => {
-
+	const updateSections = (main, sub) => {
+		setSection(main);
+		setSubSection(sub);
 	}
 
 	const sliderSettings = {
@@ -26,27 +29,54 @@ function IndexPage({ data }) {
 		className: "main_slider"
 	};
 
+	useEffect(() => {
+		if (subSection === "none") {
+			setClientsSliderActive("");
+		} else {
+			setClientsSliderActive("active");
+		}
+	}, [subSection]);
+
 	return (
 		<Layout className="Blah">
 			<div id="app">
 				<div className="inner">
 					<Slider {...sliderSettings}>
 						<div className="tileSet products">
-							{data.productsTiles.edges.map((e) => (
-								<Img fluid={e.node.childImageSharp.fluid} objectFit="contain" className="tile" />
-							))}
+							{data.productsTiles.edges.map((e) => {
+								const subsection = e.node.relativePath.replace(".png", "").replace("tile_products_", "");
+								return (
+									<a className="tile" onClick={() => { updateSections("products", subsection); }} key={e.node.childImageSharp.id}>
+										<Img fluid={e.node.childImageSharp.fluid} objectFit="contain"  />
+									</a>
+								)
+							})}
 						</div>
 						<div className="tileSet programmatic">
-							{data.programmaticTiles.edges.map((e) => (
-								<Img fluid={e.node.childImageSharp.fluid} objectFit="contain" className="tile" />
-							))}
+							{data.programmaticTiles.edges.map((e) => {
+								const subsection = e.node.relativePath.replace(".png", "").replace("tile_programmatic_", "");
+								return (
+									<a className="tile" onClick={() => { updateSections("programmatic", subsection); }} key={e.node.childImageSharp.id}>
+										<Img fluid={e.node.childImageSharp.fluid} objectFit="contain" />
+									</a>
+								)
+							})}
 						</div>
 						<div className="tileSet verticals">
-							{data.verticalsTiles.edges.map((e) => (
-								<Img fluid={e.node.childImageSharp.fluid} objectFit="contain" className="tile" />
-							))}
+							{data.verticalsTiles.edges.map((e) => {
+								const subsection = e.node.relativePath.replace(".png", "").replace("tile_verticals_", "");
+								return (
+									<a className="tile" onClick={() => { updateSections("verticals", subsection); }} key={e.node.childImageSharp.id}>
+										<Img fluid={e.node.childImageSharp.fluid} objectFit="contain" />
+									</a>
+								)
+							})}
 						</div>
 					</Slider>
+				</div>
+				<div className={classnames("sub", clientsSliderActive, section, ( subSection !== "none" ? subSection : "" ))}>
+					<SectionSlider main={section} sub={subSection} />
+					<button onClick={() => { setClientsSliderActive("") }}>X</button>
 				</div>
 			</div>
 		</Layout>
