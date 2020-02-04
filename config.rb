eba66f8b79d9aefbb5c1161f968c898b60ebabbf
Kevin Gagnon <kevin.gagnon@broadsign.com>
2020-02-04 09:34:47 -0500

    Title color and compressed CSS

--- config.rb
+++ config.rb
@@ -5,7 +5,7 @@ sass_dir = "./src/styles"
 images_dir = "./src/images/"
 
 # You can select your preferred output style here (can be overridden via the command line):
-output_style = :expanded # After dev :compressed
+output_style = :compressed # After dev :compressed
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 line_comments = true
